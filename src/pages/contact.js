c2f285fa38bdbdd54f24042c06fe0912d42436d7
sujith <sujithsuresh246@gmail.com>
2021-02-24 20:27:44 +0530

    form added

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from '../containers';
 import Layout from '../containers/Layout';
 import "../globalstyles/contact.scss"
@@ -7,28 +7,65 @@ import Name from '../images/name.svg'
 
 
 function Contact() {
+    const [formState, setFormState] = useState({
+        name: "",
+        email: "",
+    })
+
+    const encode = (data) => {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&")
+    }
+
+    const handleChange = (e) => {
+        setFormState({
+            ...formState,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({
+              "form-name": "contact",
+              ...formState
+            })
+          }).then(() => alert("Form submited!")).catch(error => alert(error))
+        e.preventDefault();
+    }
+
     return (
         <Layout>
             <div className="contactPage">
                 <Container>
                     <div className="formWrapper">
                         <h1>Let's work together!</h1>
-                        <form className="form">
+                        <form className="form" onSubmit={handleSubmit} name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                            <input type="hidden" name="form-name" value="contact"></input>
                             <div className="fieldWrapper">
                                 <label>
                                     <img src={Name} alt="name-icon"></img>
-                                    <input className="field" type="text" name="firstname" placeholder="Enter your full name"></input>
+                                    <input className="field" type="text" name="name" placeholder="Enter your full name" onChange={handleChange} value={formState.name}></input>
                                 </label>
                             </div>
                            <div className="fieldWrapper">
                             <label>
                                     <img src={Mail} alt="mail-icon"></img>
-                                    <input className="field" type="email" name="firstname" placeholder="Enter your email"></input>
-                                </label>
+                                    <input className="field" type="email" name="email" placeholder="Enter your email" onChange={handleChange} vlaue={formState.email}></input>
+                            </label>
                            </div>
-                           
+                           {/* <div>
+                                <select>
+                                    <option>Development</option>
+                                    <option>Design</option>
+                                </select>
+                            </div> */}
                             <button className="submit custom">Submit</button>
                         </form>
+                        <p>Once you have submitted your details, you will get the responce mail back within 3 days.</p>
                     </div>
                 </Container>
             </div>
