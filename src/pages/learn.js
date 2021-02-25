f92bd2b8281b914c77215ce9d8856e98849f0361
sujith <sujithsuresh246@gmail.com>
2021-02-25 13:02:49 +0530

    button style

---
+++ src/pages/learn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from '../containers';
+import Layout from '../containers/Layout';
+
+export default function Learn() {
+    return (
+        <Layout>
+            <Container>
+                
+            </Container>
+        </Layout>
+    )
+}
