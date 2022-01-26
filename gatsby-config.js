ebea3898a5ad37e68ef4ca8ad1f4d6db4c0c09fa
Sujith46 <sujithsuresh246@gmail.com>
2022-01-26 13:04:53 +0530

    fix(website): react helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React from "react"
 import "../globalstyles/style.scss"
 
 import { graphql, navigate } from "gatsby"
@@ -22,70 +22,6 @@ export default function Home({ data }) {
       }}
     />
   )
-  // let container = document.getElementById("image-wrapper")
-  // var inner = document.getElementById('sujith-img');
-  // console.log(container)
-
-  // var mouse = {
-  //   _x: 0,
-  //   _y: 0,
-  //   x: 0,
-  //   y: 0,
-  //   updatePosition: function(event) {
-  //     var e = event || window.event;
-  //     this.x = e.clientX - this._x;
-  //     this.y = (e.clientY - this._y) * -1;
-  //   },
-  //   setOrigin: function(e) {
-  //     this._x = e.offsetLeft + Math.floor(e.offsetWidth/2);
-  //     this._y = e.offsetTop + Math.floor(e.offsetHeight/2);
-  //   },
-  //   show: function() { return '(' + this.x + ', ' + this.y + ')'; }
-  // }
-  // Track the mouse position relative to the center of the container.
-  // mouse.setOrigin(container);
-
-  // const onMouseEnterHandler = (event) => {
-  //   // update(event)
-  //   console(event)
-  // }
-
-  // const onMouseLeaveHandler = () => {
-  //   inner.style = "";
-  // }
-
-  // const onMouseMoveHanlder = (event) => {
-  //   if(isTimeToUpdate()) {
-  //     // update(event);
-  //     console(event)
-  //   }
-  // }
-  // container.onmouseenter = onMouseEnterHandler;
-  // container.onmouseleave = onMouseLeaveHandler;
-  // container.onmousemove = onMouseMoveHanlder;
-
-  // var counter = 0;
-  // var updateRate = 10;
-  // var isTimeToUpdate = function() {
-  //   return counter++ % updateRate === 0;
-  // };
-
-  // const update = (event) => {
-  //   mouse.updatePosition(event);
-  //   updateTransformStyle(
-  //     (mouse.y / inner.offsetHeight/2).toFixed(2),
-  //     (mouse.x / inner.offsetWidth/2).toFixed(2)
-  //   );
-  // };
-  
-  // var updateTransformStyle = function(x, y) {
-  //   var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
-  //   inner.style.transform = style;
-  //   inner.style.webkitTransform = style;
-  //   inner.style.mozTransform = style;
-  //   inner.style.msTransform = style;
-  //   inner.style.oTransform = style;
-  // };
 
   const moveTo = () => {
     navigate('/contact');
@@ -99,7 +35,7 @@ export default function Home({ data }) {
           <div className="hero-container">
             <div className="content">
               <h1 className="typewriter-heading">{type}</h1>
-              <button className="submit" onClick={() => moveTo()}>Let's connect</button>
+              <button className="submit" onClick={moveTo}>Let's connect</button>
               <Icons />
             </div>
             <div className="image-wrapper" id="image-wrapper">
@@ -112,9 +48,6 @@ export default function Home({ data }) {
               <div className="light-yellow-box"></div>
             </div>
           </div>
-          {/* <div className={`modalWrapper modalShow-${modal}`}>
-            <p>ASDASDASDDASDA</p>
-          </div> */}
         </div>
       </Layout>
     </div>
